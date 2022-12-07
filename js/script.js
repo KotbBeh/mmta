62e4a60e64ba8a9254601c6a2928e4ef2d92e5dc
Ahmed Kotb <72921323+KotbBeh@users.noreply.github.com>
2022-12-07 15:08:51 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -101,9 +101,6 @@ function myFunction() {
 
    // Copy the text inside the text field
   navigator.clipboard.writeText(copyText.value);
-
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
 }
 
 function windowHeight() {
