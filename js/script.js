8efab77bfb3c5256a4136b1ce8a1f1c893d8a741
Ahmed Kotb <72921323+KotbBeh@users.noreply.github.com>
2022-12-07 14:58:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -91,6 +91,20 @@ function animateBox() {
         }
     });
 }
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
 
 function windowHeight() {
     $('.win-height').css({'min-height': $(window).height()});
